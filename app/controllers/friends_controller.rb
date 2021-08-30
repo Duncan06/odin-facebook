07bc411107eb7ae60fb39c8c9762810cd293975e
Rumsauce <tylerdownie0@gmail.com>
2021-08-30 13:16:19 -0700

    Add to friends controller, unsure if redirects neccessary

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,38 @@
 class FriendsController < ApplicationController
+  def create
+    @friend = Friend.new(friend_params)
+
+    if @friend.save
+      @friend.invited!
+      flash.now[:notice] = "Friend invite sent!"
+    else
+      flash.now[:alert] = "Friend invite could not be sent."
+    end
+
+  end
+
+  def update
+    @friend = Friend.find(params[:id])
+
+    if @friend && @friend.invited?
+      @friend.accepted!
+      flash.now[:notice] = "Friend invite accepted!"
+    else
+      flash.now[:notice] = "Not sent friend invite."
+    end
+
+  end
+
+  def destroy
+    @friend = Friend.find(params[:id])
+
+    @friend.destroy
+  end
+
+  private
+
+  def friend_params
+    params.permit(:user_id)
+  end
+
 end
