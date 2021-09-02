b99248363af0698ad49b14fb8d2d178c39bff698
Rumsauce <tylerdownie0@gmail.com>
2021-09-02 12:15:45 -0700

    Add guard for integration testing, and adjust buttons to only be aligned to the right

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,13 @@ class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
+  # Add red and green reporting
+  ENV['RAILS_ENV'] ||= 'test'
+  require File.expand_path('../../config/environment', __FILE__)
+  require 'rails/test_help'
+  require "minitest/reporters"
+  Minitest::Reporters.use!
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
