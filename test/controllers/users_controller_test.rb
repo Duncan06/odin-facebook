33966daf5a881bbf318b41748889b75e777bd3e8
Rumsauce <tylerdownie0@gmail.com>
2021-09-02 12:58:08 -0700

    Find fix for integration test and pg

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    get '/users/sign_in'
+    sign_in users(:user_one)
+    post user_session_url
+  end
+
+  test "Log in" do
+    get "/"
+    assert_response :success
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,4 +17,17 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include Devise::Test::IntegrationHelpers
+  include Warden::Test::Helpers 
+
+  def log_in( user )
+    if integration_test?
+      # Use warden helper
+      login_as(user, :scope => :user)
+    # Constroller_test, model_test
+    else
+      # Use devise helper
+      sign_in(user)
+    end
+  end
 end
