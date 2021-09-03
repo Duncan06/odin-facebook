23d7f773ae1a3d629e8c94cd5f988dae266ed746
Rumsauce <tylerdownie0@gmail.com>
2021-09-02 21:54:19 -0700

    Add create controller for likes, and add buttons to posts view

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,21 @@
 class LikesController < ApplicationController
   def create
+    @like = Like.new(like_params)
+    @like.user_id = current_user.id 
+    @like.save
+
+    flash[:notice] = "Like submitted"
+
+    redirect_back fallback_location: posts_url
   end
 
   def destroy
+  end
+
+  private
+
+  def like_params
+    params.permit(:post_id)
   end
 
 end
