2e0569b1509c3758e407b0b86e1af4ffece33356
Rumsauce <tylerdownie0@gmail.com>
2021-09-03 14:12:35 -0700

    Start comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+    
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post_id = params[:post_id]
+
+    @comment.save
+
+    flash[:notice] = "Comment created!"
+
+    redirect_back fallback_location: posts_url
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author_name, :body)
+  end
 end
