e018d179ea20b6c6d2d42a67e819ce9968f2f4a9
Rumsauce <tylerdownie0@gmail.com>
2021-09-09 11:11:18 -0700

    Add spacing to buttons

--- db/migrate/20210904045159_add_post_id_to_comment.rb
+++
@@ -1,5 +0,0 @@
-class AddPostIdToComment < ActiveRecord::Migration[6.1]
-  def change
-    add_column :comments, :post_id, :bigint
-  end
-end
--- db/migrate/20210904183321_drop_comment.rb
+++
@@ -1,9 +0,0 @@
-class DropComment < ActiveRecord::Migration[6.1]
-  def up
-    drop_table :comments
-  end
-
-  def down
-    raise ActiveRecorde::IrreversibleMigration
-  end
-end
